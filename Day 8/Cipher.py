c0d72e22c2e793698a7fac534414a381e7a063f1
Saptarshi <69756936+Devil-Rick@users.noreply.github.com>
2022-07-14 02:38:46 +0530

    Update Cipher.py

--- Day 8/Cipher.py
+++ Day 8/Cipher.py
@@ -1,7 +1,8 @@
 def cipher(word, todo, c_num):
     cipher_text = ""
-    if todo == "encode":
-        for i in word:
+    if todo == "decode":
+        c_num *= -1
+    for i in word:
             char = i
             if i.islower():
                 cipher_text += chr((ord(char) - 97 + c_num) % 26 + 97)
@@ -9,17 +10,6 @@ def cipher(word, todo, c_num):
                 cipher_text += chr((ord(char) - 65 + c_num) % 26 + 65)
             elif i.isdigit():
                 cipher_text += chr((ord(char) - 48 + c_num) % 10 + 48)
-            else:
-                cipher_text += char
-    else:
-        for i in word:
-            char = i
-            if i.islower():
-                cipher_text += chr((ord(char) - 97 - c_num) % 26 + 97)
-            elif i.isupper():
-                cipher_text += chr((ord(char) - 65 - c_num) % 26 + 65)
-            elif i.isdigit():
-                cipher_text += chr((ord(char) - 48 - c_num) % 10 + 48)
             else:
                 cipher_text += char
     return cipher_text
