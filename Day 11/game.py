f0b8e626ae3c8ead05b1dad4110933c03fc14e99
Saptarshi <69756936+Devil-Rick@users.noreply.github.com>
2022-07-16 20:39:44 +0530

    Add files via upload

--- Day 11/game.py
+++ Day 11/game.py
@@ -25,6 +25,11 @@ def results(cards):
     computer_score = 0
     for i in computer:
         computer_score += cards[i]
+    while computer_score <= 16:
+        computer_card = rn.sample(cards.keys(), 1)
+        for i in computer_card:
+            computer.append(i)
+            computer_score += cards[i]
     won = winner(computer_score, final_score)
     return (f"\nComputer's final hand: {computer} , computer final score: {computer_score}\n\n{won}")
 
@@ -33,18 +38,22 @@ def show_hand(cards):
     computer_score = 0
     for i in computer:
         computer_score += cards[i]
+    while computer_score <= 16:
+        computer_card = rn.sample(cards.keys(), 1)
+        for i in computer_card:
+            computer.append(i)
+            computer_score += cards[i]
     won = winner(computer_score, total_player)
     return (f"\n\nYour cards : {player[:2]} , your final score {total_player} \nComputer's final hand: {computer} , computer final score: {computer_score} \n\n{won}")
 
 
 def winner(c_score, p_score):
-    if "A" in player:
-        check = input(
-            "\n\nDo you want value of A as 11 or 1 \n If 11 press '11' otherwise press '1' : ")
-        if check == 1:
-            p_score -= 10
-
-    if p_score > 21:
+    if p_score == c_score and c_score == 21:
+        return f"Looks like the dealer got Black Jack \n {art.lose}"
+    elif p_score == c_score and c_score == 21:
+        return f"It's Draw .... Better luck next time \n {art.draw}"
+    elif p_score > 21:
+        p_score -= 10
         return f"You went over 21. You lose 😭 \n {art.lose}"
     elif p_score < c_score:
         return f"You lose ....Wrong Choice it seems .... \n {art.lose}"
