850433ad8faf1abcb84ab836d5c94d74cacb4ba3
Saptarshi <69756936+Devil-Rick@users.noreply.github.com>
2022-08-11 17:47:39 +0530

    Delete Rain Alert Notification App directory

--- Rain Alert Notification App/config.py
+++
@@ -1,5 +0,0 @@
-OW_api_id = "https://api.openweathermap.org/data/2.5/onecall"
-OW_api_key = "8e00e4c9248edc4799d3c163fe18d89e"
-
-TW_auth_sid = "AC5e0ab2d87bcb40ff340cbecab3536d2e"
-TW_auth_token = "22091fd64a3de4eeb947c113269ebb06"
